23e5615c6af347a002f1beb91e25a79107841cc0
u-kaushik <u.kaushik@hotmail.co.uk>
2021-10-05 13:41:25 +0100

    init

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "Iv1.a105ba23b457f83d";
+    this.client_secret = "d13c695b5c1190a487333352852c4b52b0594bb6";
+    this.repos_count = 10;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
